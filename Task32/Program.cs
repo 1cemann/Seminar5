3be42e23c48ddba8465d56b1dc653209b6e374d0
Mad <ned_91@mail.ru>
2022-06-20 21:03:26 +0300

    add Task 32 Task 33

---
+++ Task32/Program.cs
@@ -0,0 +1,40 @@
+﻿//**Задача 32:** Напишите программу замена элементов массива: положительные 
+//элементы замените на соответствующие отрицательные, и наоборот.
+
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+using System;
+using static System.Console;
+Clear();
+
+
+int size=int.Parse(ReadLine());
+int Max=int.Parse(ReadLine());
+int Min=int.Parse(ReadLine());
+int[] array= new int[size];
+array=GetRandomArray(size,Min,Max);
+WriteLine($"Массив= {string.Join(",",array)}");
+WriteLine($"Массив инверсный= {string.Join(",",GetReversPositivAraay(array))}");
+
+
+int[] GetRandomArray(int size,int MinValue,int MaxValue)
+{
+    int[] result=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        result[i]=new Random().Next(MinValue,MaxValue+1);
+    }
+    return result;
+}
+
+int[] GetReversPositivAraay(int[] array)
+{
+    int size=array.Length;
+    int []ar= new int[size];
+    for(int i=0;i<size;i++)
+    {
+        ar[i]=array[i]*-1;
+    }
+    return ar;
+}
---
+++ Task33/Program.cs
@@ -0,0 +1,39 @@
+﻿//**Задача 33:** Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+
+//4; массив [6, 7, 19, 345, 3] -> нет
+
+//3; массив [6, 7, 19, 345, 3] -> да
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите длину массива   ");
+int size=int.Parse(ReadLine());
+Write("Введите Мах значение в массиве   ");
+int Max=int.Parse(ReadLine());
+Write("Введите Мин значение в  массиве   ");
+int Min=int.Parse(ReadLine());
+Write("Введите искомый элемент   ");
+int El=int.Parse(ReadLine());
+int[] array= new int[size];
+array=GetRandomArray(size,Min,Max);
+WriteLine($"Массив= {string.Join(",",array)}");
+if(GetFindElement(array,El)==true){WriteLine($"Данный елемент присутствует в массиве");}else{
+WriteLine($"Данный елемент отсутствует в массиве");}
+
+
+int[] GetRandomArray(int size,int MinValue,int MaxValue){
+    int[] result=new int[size];
+    for(int i=0;i<size;i++){
+        result[i]=new Random().Next(MinValue,MaxValue);
+    }
+    return result;
+}
+
+bool GetFindElement(int[] array,int Element){
+    for(int i=0;i<array.Length;i++){
+        if(array[i]==Element){return true;}
+    }
+    return false;
+}
